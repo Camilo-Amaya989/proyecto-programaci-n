84b9add2a1f5d64120cbcb57d8b230800f863151
laura-moreno <lauravalentinamoreno89@gmail.com>
2024-08-14 15:03:32 -0500

    Add files via upload

---
+++ act.py
@@ -0,0 +1,15 @@
+#entrada=variables
+#proceso=codigo
+#salida=print
+def pedir_elemento (lista):
+    lista.append("Jabon")
+
+
+
+#empieza
+lista_productos=[]
+#llamo la funcion        
+pedir_elemento( lista_productos) #se ve arriba
+print(lista_productos)
+    
+
